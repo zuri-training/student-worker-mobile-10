459eb333921c6c0908ac589bc12d99412d54dce5
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2021-06-29 10:17:18 -0700

    new commit

--- lib/views/screens/jobMain.dart
+++ lib/views/screens/jobMain.dart
@@ -64,6 +64,10 @@ class _JobMainstate extends State<JobMain> {
             decoration: InputDecoration(
               border: InputBorder.none,
               contentPadding: EdgeInsets.all(10),
+              suffixIcon: Icon(
+                Icons.search,
+                color: Colors.black45,
+              ),
               hintText: 'Title/Keyword/Category',
               hintStyle: TextStyle(
                 color: Colors.black38,
