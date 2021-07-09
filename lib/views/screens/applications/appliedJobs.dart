76adf889487ad7b51858f46af51f19a20c7e8eb5
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2021-07-09 12:23:13 -0700

    created by katherine obide

--- lib/views/screens/applications/appliedJobs.dart
+++ lib/views/screens/applications/appliedJobs.dart
@@ -1,3 +1,4 @@
+//linked to applications.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:student_worker/applications/applicationProvider.dart';
